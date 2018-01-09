436ac0a7fcb2ccc9e15b0514f0f716105a1e8c99
Jesse Lansdown <jesselansdown@gmail.com>
2018-01-09 17:47:20 +0100

    Initial commit

---
+++ CanonsAndAuts.c
@@ -0,0 +1,9 @@
+To do:
+
+Read in a graph in .g6 format
+optionally read in a file with colourings
+output a file with the canonical representations - particularly of a specific coloured section? and also a file with the automorphisms
+Put flags to only output one or the other, and do both by default
+output in the form of a list, readable by gap
+flags for choosing to 0 index the input and output respectively, default being to start at 1
+
